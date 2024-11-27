60a91447acaf317029d8df7a7367d1c2b8bdbc37
RacheleBurgio <rachele.burgio32@gmail.com>
2024-11-27 16:50:05 +0100

    finiti anche ultimi punti

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,56 @@
+import { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 
-const SingleBook = ({ book }) => {
-  return (
-    <Card className="h-100" style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={book.img} alt={book.title} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  )
+// const SingleBook = ({ book }) => {
+//   return (
+//     <Card className="h-100" style={{ width: '18rem', margin: '10px' }}>
+//       <Card.Img variant="top" src={book.img} alt={book.title} />
+//       <Card.Body>
+//         <Card.Title>{book.title}</Card.Title>
+//       </Card.Body>
+//     </Card>
+//   )
+// }
+
+// export default SingleBook
+
+class SingleBook extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selected: false,
+    }
+  }
+
+  handleSelectToggle = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }))
+  }
+
+  render() {
+    const { book } = this.props
+    const { selected } = this.state
+
+    return (
+      <Card
+        className={`h-100 ${selected ? 'border border-danger' : ''}`}
+        style={{ width: '18rem', margin: '10px' }}
+        onClick={this.handleSelectToggle}
+      >
+        <Card.Img
+          variant="top"
+          src={book.img}
+          alt={book.title}
+          style={{ cursor: 'pointer' }}
+        />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    )
+  }
 }
 
 export default SingleBook
